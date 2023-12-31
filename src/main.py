b6b0cf19d72c44450864baf080b0dc4eae33d220
BenSiv <bensiv92@gmail.com>
2023-12-31 13:58:08 +0200

    updating main.py

--- src/main.py
+++ src/main.py
@@ -4,9 +4,14 @@ Main script to build database from public data
 
 # dependencies
 import os
+import json
 from download_icgd_collection import save_collection
 from update_nacode import update_nacodes
 from scrape_clone_info import scrape_info
+
+def save_as_json(data, output_file):
+    with open(output_file, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
 
 def main():
     script_path = os.path.realpath(__file__)
@@ -18,7 +23,10 @@ def main():
     # retrives nacodes based on clonename search
     update_nacodes(collection_file)
     # retrives all the data based on the collection file, saved as data.json
-    scrape_info(collection_file)
+    result = scrape_info(collection_file)
+
+    output_file = os.path.join(project_path, "data", "data.json")
+    save_as_json(result, output_file)
 
 if __name__ == "__main__":
     main()
