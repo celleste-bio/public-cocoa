7f6090daf217bae8495a22cea69210e4f7892493
BenSiv <bensiv92@gmail.com>
2024-06-25 11:26:33 +0300

    adding port to localhost

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker stop public-cocoa
-docker remove public-cocoa
+docker rm public-cocoa
 docker image remove public-cocoa
 
 docker build --tag public-cocoa .
@@ -9,5 +9,6 @@ docker run \
     --detach --tty \
     --name public-cocoa \
     --hostname public-cocoa \
+    --publish 8080:8080 \
     public-cocoa \
     bash
